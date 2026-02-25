1142e51938ba2319dd54d331097a5b996d29ec91
Krishna <neeturaikwar914@gmail.com>
2026-02-26 01:41:36 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./",        // relative path assets ke liye
   plugins: [react()],
-  build: {
-    outDir: "dist"   // ensure dist folder create ho
-  },
   server: {
-    host: "0.0.0.0",
-    port: 10000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://kri-lion.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
-});
+})
